083b7b0054379e3cc1d4409dd3602ce02c4e1f87
paul <paultaveneaux@gmail.com>
2022-04-08 18:25:30 +0200

    added SDL

---
+++ fizzbuzz.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    fprintf(stderr, "[ERROR] to many args provided ! (%i)\n", argc);
+    exit(1);
+  }
+
+  int iter = atoi(argv[1]);
+
+  printf("%i\n", iter);
+
+  for (int i = 1; i <= iter; ++i)
+  {
+    if (i % 3 != 0 && i % 5 != 0)
+      printf("%i", i);
+    else
+    {
+      if (i % 3 == 0)
+      {
+        printf("fizz");
+      }
+      if (i % 5 == 0)
+      {
+        printf("buzz");
+      }
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
